b58e42e94d4be20abe58272cdf4219ec1ec3ac4c
Mohd Yameen <myameend@gmail.com>
2024-01-23 00:09:36 +0530

    Added user

--- src/main/java/com/spring/springApi/service/UserService.java
+++ src/main/java/com/spring/springApi/service/UserService.java
@@ -17,13 +17,14 @@ public class UserService {
     public UserService(){
         userList = new ArrayList<>();
 
-        User user1 = new User(1, "Sahil", 26, "mohdsahil@gmail.com");
+        User user1 = new User(1, "Nandini", 26, "nandini@gmail.com");
         User user2 = new User(2, "Yameen", 26, "yameen@gmail.com");
-        User user3 = new User(3, "Danish", 29, "danish@gmail.com");
-        User user4 = new User(4, "Atif", 29, "atif@gmail.com");
-        User user5 = new User(5, "Shavez", 28, "shavez@gmail.com");
+        User user3 = new User(3, "Sahil", 26, "mohdsahil@gmail.com");
+        User user4 = new User(3, "Danish", 29, "danish@gmail.com");
+        User user5 = new User(4, "Atif", 29, "atif@gmail.com");
+        User user6 = new User(5, "Shavez", 28, "shavez@gmail.com");
 
-        userList.addAll(Arrays.asList(user1, user2, user3, user4, user5));
+        userList.addAll(Arrays.asList(user1, user2, user3, user4, user5, user6));
     }
 
     public Optional<User> getUser(Integer id){
